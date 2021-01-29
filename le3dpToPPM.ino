6ef569a36a960f2fd2368ef8591c20dc0307f5fb
lexfp <lexfp@nowhere.com>
2021-01-29 10:49:58 -0500

    alpha

---
+++ le3dpToPPM.ino
@@ -0,0 +1,99 @@
+/* Simplified Logitech Extreme 3D Pro Joystick Report Parser */
+
+#include <usbhid.h>
+#include <hiduniversal.h>
+#include <usbhub.h>
+
+#include "le3dp_rptparser.h"
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+//PPM config values
+#define FRAME_LENGTH 22500  //set the PPM frame length in microseconds (1ms = 1000µs)
+#define PULSE_LENGTH 300  //set the pulse length
+#define onState 1  //set polarity of the pulses: 1 is positive, 0 is negative
+#define sigPin 3  //set PPM signal output pin on the arduino
+
+BtnPPMMap btnPPMMap;
+USB                                             Usb;
+USBHub                                          Hub(&Usb);
+HIDUniversal                                    Hid(&Usb);
+JoystickEvents                                  JoyEvents(&btnPPMMap);
+JoystickReportParser                            Joy(&JoyEvents);
+
+void setup()
+{
+  Serial.begin( 115200 );
+#if !defined(__MIPSEL__)
+  while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+  Serial.println("Start");
+
+  if (Usb.Init() == -1)
+    Serial.println("OSC did not start.");
+
+  delay( 200 );
+
+  if (!Hid.SetReportParser(0, &Joy))
+    ErrorMessage<uint8_t>(PSTR("SetReportParser"), 1  );
+
+  pinMode(sigPin, OUTPUT);
+  digitalWrite(sigPin, !onState);  //set the PPM signal pin to the default state (off)
+
+  cli();
+  TCCR1A = 0; // set entire TCCR1 register to 0
+  TCCR1B = 0;
+
+  OCR1A = 100;  // compare match register, change this
+  TCCR1B |= (1 << WGM12);  // turn on CTC mode
+  TCCR1B |= (1 << CS11);  // 8 prescaler: 0,5 microseconds at 16mhz
+  TIMSK1 |= (1 << OCIE1A); // enable timer compare interrupt
+  sei();
+}
+
+void loop()
+{
+  Usb.Task();
+  if (Usb.getUsbTaskState() != USB_STATE_RUNNING)
+  {
+    //disconnected joystick
+    JoyEvents.disconnectJoystick();
+    //Serial.println("Joystick disconnected");
+  }
+}
+
+ISR(TIMER1_COMPA_vect) { //leave this alone
+  static boolean state = true;
+
+  TCNT1 = 0;
+
+  if (state) {  //start pulse
+    digitalWrite(sigPin, onState);
+    OCR1A = PULSE_LENGTH * 2;
+    state = false;
+  } else { //end pulse and calculate when to start the next pulse
+    static byte cur_chan_numb;
+    int channelValue;
+    static unsigned int calc_rest;
+
+    digitalWrite(sigPin, !onState);
+    state = true;
+
+    if (cur_chan_numb >= BtnPPMMap::NUM_CHANNELS) {
+      cur_chan_numb = 0;
+      calc_rest = calc_rest + PULSE_LENGTH;//
+      OCR1A = (FRAME_LENGTH - calc_rest) * 2;
+      calc_rest = 0;
+    }
+    else {
+      channelValue = btnPPMMap.getChannelValue(cur_chan_numb);
+      OCR1A = (channelValue - PULSE_LENGTH) * 2;
+      calc_rest = calc_rest + channelValue;
+      cur_chan_numb++;
+    }
+  }
+}
